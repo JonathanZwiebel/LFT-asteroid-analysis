ca845a177b5ff5a321e26049b8a75c19beefa4d3
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2015-09-23 18:24:14 -0700

    Fixed filename exception and tested basic subtractive version

--- src/main/SimpleSubtractive.java
+++ src/main/SimpleSubtractive.java
@@ -4,21 +4,24 @@ import java.io.IOException;
 
 import nom.tam.fits.Fits;
 import nom.tam.fits.FitsException;
+import nom.tam.fits.TableHDU;
 
 public class SimpleSubtractive {
 	// index from 1 to match fits file
 	public static final int BASE_IMAGE_INDEX = 1;
 	public static final int TOP_IMAGE_INDEX = 11; 
-	public static final String FILESYSTEM_LOC = "C:\\Users\\user\\Desktop";
-	public static final String FILENAME = "ktwo200000905_c00_lpd-targ.fits";
+	public static final String COLUMN = "FLUX";
+	public static final String FILENAME = "C:\\Users\\user\\Desktop\\ktwo200000905-c00_lpd-targ.fits";
 
 	public static void main(String[] args) {
 		try {
-			Fits fits = readFile(FILESYSTEM_LOC + "\\" + FILENAME);
-			float[][][] fits_data_cube_mat = extractDataCube(fits);
-
-			float[][] base_image_mat = extractDataSliceFromFmat(fits_data_cube_mat, BASE_IMAGE_INDEX);
-			float[][] top_image_mat = extractDataSliceFromFmat(fits_data_cube_mat, TOP_IMAGE_INDEX);
+			Fits fits = readFile(FILENAME);
+			TableHDU<?> thdu = extractTable(fits, 1);
+			
+			float[][][] data_cube_mat = (float[][][]) thdu.getColumn(COLUMN);
+			
+			float[][] base_image_mat = extractDataSlice(data_cube_mat, BASE_IMAGE_INDEX);
+			float[][] top_image_mat = extractDataSlice(data_cube_mat, TOP_IMAGE_INDEX);
 
 			float[][] subtracted_image_mat = subtractImages(top_image_mat, base_image_mat);
 
@@ -37,16 +40,16 @@ public class SimpleSubtractive {
 	}
 
 	// converts from fits object to 3d float matrix
-	public static float[][][] extractDataCube(Fits fits) throws FitsException, IOException {
-		return (float[][][]) fits.getHDU(1).getData().getKernel();
+	public static TableHDU<?> extractTable(Fits fits, int HDU_index) throws FitsException, IOException {
+		return (TableHDU<?>) fits.getHDU(HDU_index);
 	}
 
-	// extracts a 2d float matrix from a 3d one (represets a slice in time)
-	public static float[][] extractDataSliceFromFmat(float[][][] data_cube_mat, int image_index) {
-		return data_cube_mat[image_index - 1];
+	// extracts a 2d float matrix from a 3d one (represents a slice in time)
+	public static float[][] extractDataSlice(float[][][] data_cube_mat, int index) {
+		return data_cube_mat[index - 1];
 	}
 
-	// subtracts two 2d slices from eacther top-base and outputs result
+	// subtracts two 2d slices from eacother top-base and outputs result
 	public static float[][] subtractImages(float[][] top_image_mat, float[][] base_image_mat) {
 		int row_count = top_image_mat.length;
 		int column_count = top_image_mat[0].length;
