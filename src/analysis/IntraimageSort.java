0d87e272f6e11e16bcbe5bc51a25bdf7b9feaef0
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2015-10-14 19:09:52 -0700

    Added hasmap sorting by value to position

--- src/analysis/IntraimageSort.java
+++ src/analysis/IntraimageSort.java
@@ -2,8 +2,11 @@ package analysis;
 
 import stars.PixelPoint;
 
+import java.util.List;
+import java.util.Map;
 import java.util.HashMap;
-import static java.util.Map.Entry.comparingByValue;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 /**
  * Sorts through an image and returns a PixelPoint array of the coordinates brightness
@@ -12,6 +15,17 @@ import static java.util.Map.Entry.comparingByValue;
 public class IntraimageSort {
     public PixelPoint[] sortedArray(float[][] image) {
         // mess with load factor
-        HashMap<Integer, PixelPoint> hashmap = new HashMap(2500, 0.75);
+        HashMap<PixelPoint, Float> hashmap = new HashMap<>(2500, 0.75f);
+        for(int i = 0; i < image.length; i++) {
+            for(int j = 0; j < image[0].length; j++) {
+                hashmap.put(new PixelPoint(i, j), image[i][j]);
+            }
+        }
+        sortByValue(hashmap);
+        return (PixelPoint[]) hashmap.values().toArray();
+    }
+
+     <K, V extends Comparable<? super V>> List<Entry<K, V>> sortByValue(Map<K, V> map) {
+        return map.entrySet().stream().sorted(Entry.comparingByValue()).collect(Collectors.toList());
     }
 }
