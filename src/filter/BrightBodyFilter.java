f03d05e086498f7ad91240dbddcf20b079a35023
JonathanZwiebel <jonathanzwiebel@gmail.com>
2015-10-23 04:26:36 -0700

    Updated references for new laptop

--- src/filter/BrightBodyFilter.java
+++ src/filter/BrightBodyFilter.java
@@ -8,8 +8,8 @@ import nom.tam.fits.Fits;
  * This class filters a FITS image into just the stars
  */
 public class BrightBodyFilter {
-    public static final String INPUT_FILENAME = "C:\\Users\\user\\Desktop\\K2\\raw\\ktwo200000908-c00_lpd-targ.fits";
-    public static final String OUTPUT_HEAD = "C:\\Users\\user\\Desktop\\K2\\filtered\\ktwo200000908-c00";
+    public static final String INPUT_FILENAME = "C:\\Users\\admin\\Desktop\\K2\\raw\\ktwo200000905-c00_lpd-targ.fits";
+    public static final String OUTPUT_HEAD = "C:\\Users\\admin\\Desktop\\K2\\filtered\\ktwo200000908-c00";
     public static final String COLUMN = "FLUX";
 
     public static final int BINARY_THRESHOLD = 1000;
