2a5189af1c653c822cecf0c0cab3051816260cad
JonathanZwiebel <jonathanzwiebel@gmail.com>
2015-10-28 18:39:07 -0700

    Documented ImageStats, IntraimageSort and removed MomentTester

--- src/analysis/ImageStats.java
+++ src/analysis/ImageStats.java
@@ -1,17 +1,65 @@
 package analysis;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * This class finds various statistics of an image by treating each pixel value as a homogenous data point
+ * TODO: Safely remove this class or make it non-instantiatable
+ */
 public class ImageStats {
+    /**
+     * The image that this class performs statistics on
+     */
     float[][] image;
-    ArrayList<Float> data_list;
-    public float mean, std_dev, min, q1, med, q3, max;
 
+    /**
+     * An ArrayList of the data in the iamge
+     */
+    ArrayList<Float> data_list;
+
+    /**
+     * The mean of the data
+     */
+    public float mean;
+
+    /**
+     * The standard deviation of the data
+     */
+    public float std_dev;
+
+    /**
+     * The minimum value of the data
+     */
+    public float min;
+
+    /**
+     * The first quartile of the data
+     */
+    public float q1;
+
+    /**
+     * The median of the data
+     */
+    public float med;
+
+    /**
+     * The third quartile of the data
+     */
+    public float q3;
+
+    /**
+     * The maximum value of the data
+     */
+    public float max;
+
+    /**
+     * Constructs an instance of ImageStats with a given image
+     * @param image the input image
+     */
     public ImageStats(float[][] image) {
         this.image = image;
-        ArrayList<Float> data_list = new ArrayList<Float>();
+        ArrayList<Float> data_list = new ArrayList();
         for(float[] arrayf : image) {
             for(float f : arrayf) {
                 data_list.add(f);
@@ -21,12 +69,18 @@ public class ImageStats {
         analyze();
     }
 
+    /**
+     * Abstraction to calculate the desired values
+     */
     private void analyze() {
         findMean();
         findStdDev();
         findQuartiles();
     }
 
+    /**
+     * Calculates and sets the mean of the data
+     */
     private void findMean() {
         float sum = 0;
         for(float f : data_list) {
@@ -35,6 +89,9 @@ public class ImageStats {
         mean =  sum / data_list.size();
     }
 
+    /**
+     * Finds and sets min, q1, med, q3, and max
+     */
     private void findQuartiles() {
         int size = data_list.size();
         min = data_list.get(0);
@@ -44,6 +101,10 @@ public class ImageStats {
         max = data_list.get(size - 1);
     }
 
+    /**
+     * Calculates and sets the standard deviation of the data
+     * The mean must be calculated before
+     */
     private void findStdDev() {
         float std_dev_squared = 0;
         for(float f : data_list) {
@@ -51,19 +112,5 @@ public class ImageStats {
         }
         std_dev_squared /= data_list.size();
         std_dev = (float) Math.sqrt(std_dev_squared);
-    }
-
-    public static float mean(float[][] image) {
-        ArrayList<Float> data_list = new ArrayList();
-        for(float[] arrayf : image) {
-            for(float f : arrayf) {
-                data_list.add(f);
-            }
-        }
-        float sum = 0;
-        for(float f : data_list) {
-            sum += f;
-        }
-         return sum / data_list.size();
     }
 }
--- src/analysis/IntraimageSort.java
+++ src/analysis/IntraimageSort.java
@@ -1,21 +1,22 @@
 package analysis;
 
 import stars.PixelPoint;
-
-import java.util.List;
-import java.util.Map;
 import java.util.HashMap;
-import java.util.Map.Entry;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 /**
- * Sorts through an image and returns a PixelPoint array of the coordinates brightness
- * TODO: SERIALIZE AND OPTIMIZE SORING
+ * Has a static method to through an image and return a PixelPoint array of the coordinates brightness
+ * TODO: Safely remove this
  */
 public class IntraimageSort {
+    public static final float SHIFT_VALUE  = 0.0001f;
+
+    /**
+     * Returns the sorted array of PixelPoints by their value in the image
+     * @param image the input image
+     * @return the sorted array of PixelPoints
+     */
     public static PixelPoint[] sortedArray(float[][] image) {
-        // mess with load factor
         HashMap<Float, PixelPoint> hashmap = new HashMap<>(2500, 0.75f);
         for (int i = 0; i < image.length; i++) {
             for (int j = 0; j < image[0].length; j++) {
@@ -30,11 +31,15 @@ public class IntraimageSort {
         return sortedPixelPoints;
     }
 
-    // deals with repetitions by slightly modifying values (sketchy)
-    public static void addToHashMap(HashMap hashmap, float value, PixelPoint point) {
+    /**
+     * Adds a specific value to a hashmap, modifying them slightly if needed
+     * @param hashmap the hashmap to add to
+     * @param value the value to add
+     * @param point the corresponding PixelPoiint
+     */
+    private static void addToHashMap(HashMap hashmap, float value, PixelPoint point) {
         while(hashmap.containsKey(value)){
-            System.out.println("Duplicate keys of " + value + " at i = " + point.x + " j = " + point.y);
-            value += 0.0001f;
+            value += SHIFT_VALUE;
         }
         hashmap.put(value, point);
     }
