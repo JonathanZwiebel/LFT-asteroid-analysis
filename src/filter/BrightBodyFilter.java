941517117435fc72e98753d427e64893e9602152
JonathanZwiebel <jonathanzwiebel@gmail.com>
2015-11-02 17:58:40 -0800

    Documented the BinaryFilter

--- src/filter/BrightBodyFilter.java
+++ src/filter/BrightBodyFilter.java
@@ -8,6 +8,18 @@ import java.io.IOException;
  * Interface for filtering a TrackBrightBody
  */
 public interface BrightBodyFilter {
+    /**
+     * Generates and writes the data cube
+     * @throws FitsException
+     * @throws IOException
+     */
     void writeFilter() throws FitsException, IOException;
+
+    /**
+     * Generates and returns the filtered data cube
+     * @return the filtered data cube
+     * @throws FitsException
+     * @throws IOException
+     */
     float[][][] filter() throws FitsException, IOException;
 }
--- src/filter/MeanBrightBodyFilter.java
+++ src/filter/MeanBrightBodyFilter.java
@@ -9,17 +9,29 @@ import java.io.IOException;
 
 
 /**
- * This class filters a FITS image by doing a positive binary filter over the mean
+ * This class filters a FITS image by doing a positive binary filter over the mean value of the image
  */
 public class MeanBrightBodyFilter implements BrightBodyFilter {
     String input_filename_, output_head_, column_;
 
+    /**
+     * Constructs a MeanBrightBodyFilter object
+     * @param input_filename BrightBody file
+     * @param output_head Filtered output file
+     * @param column Column to filter
+     */
     public MeanBrightBodyFilter(String input_filename, String output_head, String column) {
         this.input_filename_ = input_filename;
         this.output_head_ = output_head;
         this.column_ = column;
     }
 
+    /**
+     * Generates and returns the filtered data cube
+     * @return the filtered data cube
+     * @throws FitsException
+     * @throws IOException
+     */
     public float[][][] filter() throws FitsException, IOException {
         Fits f = FitsHelper.readFile(input_filename_);
         float[][][] column = FitsHelper.extractFilteredColumn(f, column_);
@@ -27,6 +39,11 @@ public class MeanBrightBodyFilter implements BrightBodyFilter {
         return ArrayHelper.intToFloat(filtered);
     }
 
+    /**
+     * Generates and writes the filtered datat cube
+     * @throws FitsException
+     * @throws IOException
+     */
     public void writeFilter() throws FitsException, IOException {
         Fits f = FitsHelper.readFile(input_filename_);
         float[][][] column = FitsHelper.extractFilteredColumn(f, column_);
