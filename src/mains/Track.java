a6952bb8542231bc829cc6a01d98a6f895fdc176
JonathanZwiebel <jonathanzwiebel@gmail.com>
2015-11-25 19:53:59 -0800

    Tested and reformated main to work with new preprocessor and data director

--- src/mains/Track.java
+++ src/mains/Track.java
@@ -12,11 +12,11 @@ import java.io.File;
  *
  */
 public class Track {
-    public static final String DATA_FILENAME =          "data\\raw\\ktwo200000905-c00_lpd-targ.fits";
+    public static final String DATA_FILENAME =          "c:\\users\\admin\\desktop\\k2\\raw\\ktwo200000908-c00_lpd-targ.fits";
     public static final String TEXT_DIRECTORY =         "data\\text\\908";
     public static final String SERIALIZED_DIRECTORY =   "data\\ser\\908";
     public static final String CSV_DIRECTORY =          "data\\csv\\908";
-    public static final String MASS_CSV_FILENAME =      "data\\csv\\905MassAreaSorted.csv";
+    public static final String MASS_CSV_FILENAME =      "data\\csv\\908MassAreaSorted.csv";
     public static final String COLUMN =                 "FLUX";
 
     public static void main(String[] args) {
