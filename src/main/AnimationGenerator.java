e0f52345d170005e84a71785acae807857d9475b
JonathanZwiebel <jonathanzwiebel@gmail.com>
2015-11-25 18:02:54 -0800

    Added some more animations

--- src/main/AnimationGenerator.java
+++ src/main/AnimationGenerator.java
@@ -8,8 +8,8 @@ import write.SimpleWriter;
  * This class will generate an animation with a given column and data set
  */
 public class AnimationGenerator {
-    public static final String INPUT_FILENAME = "c:\\users\\admin\\desktop\\k2\\raw\\ktwo200000908-c00_lpd-targ.fits";
-    public static final String OUTPUT_FILENAME = "data\\ani\\908.fits";
+    public static final String INPUT_FILENAME = "c:\\users\\admin\\desktop\\k2\\raw\\ktwo200000909-c00_lpd-targ.fits";
+    public static final String OUTPUT_FILENAME = "data\\ani\\909.fits";
 
 
     public static void main(String[] args) {
