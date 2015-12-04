e30c5e7ca8bf69b78cbe64710c861570922aaa5e
JonathanZwiebel <jonathanzwiebel@gmail.com>
2015-12-03 21:09:14 -0800

    Wrote locator and locator instance

--- src/locating/Locator.java
+++ src/locating/Locator.java
@@ -1,16 +1,44 @@
 package locating;
 
+import brightbodies.BrightBodyList;
+
 /**
+ * Abstract class to locate the BrightBodies in individual indices of a data cube and return them as an array of
+ * BrightBodyLists
+ *
  * @author Jonathan Zwiebel
  * @version November 30th, 2015
  */
 public abstract class Locator {
     protected float[][][] data_;
+    protected LocatorInstance[] instances_;
 
+    /**
+     * Constructs a Locator object given a cleaned data cube from a preprocessor
+     * @param data floating point data cube from a preprocessor
+     */
     public Locator(float[][][] data) {
         data_ = data;
     }
 
-    public abstract TrackerInstance[] locate();
+    /**
+     * Initializes the instances_ variable to unique LocatorInstances for each timestamp
+     * TODO: Put this in the constructor, will require subclass to have reference to subLocatorInstance class
+     */
+    public abstract void generateInstances();
 
+    /**
+     * Locates the BrightBodies in each instance by delegating the task to the LocatorInstances
+     * @return array of BrightBodyLists corresponding to BrightBodies at each index
+     * TODO: Consider not using BrightbodyList
+     */
+    public BrightBodyList[] locate() {
+        BrightBodyList[] bright_body_lists = new BrightBodyList[data_.length];
+
+        for(int index = 0; index < data_.length; index++) {
+            bright_body_lists[index]  = instances_[index].locate();
+        }
+
+        return bright_body_lists;
+    }
 }
--- src/locating/LocatorInstance.java
+++ src/locating/LocatorInstance.java
@@ -1,8 +1,25 @@
 package locating;
 
+import brightbodies.BrightBodyList;
+
 /**
  * @author Jonathan Zwiebel
  * @version November 30th, 2015
  */
 public abstract class LocatorInstance {
+    float[][] data_;
+
+    /**
+     * Constructs a LocatorInstance given an image array from the data cube, to be called by a Locator object
+     * @param data one index of a data cube
+     */
+    public LocatorInstance (float[][] data) {
+        data_ = data.clone();
+    }
+
+    /**
+     * Locates all of the BrightBodies in the data array
+     * @return BrightBodyList showing locations of all of the Brightbodies
+     */
+    public abstract BrightBodyList locate();
 }
