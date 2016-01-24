d62a9fc09d23feb57c4efe6ef04a955bf788d312
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2016-01-24 01:27:06 -0800

    Moved and added tets

---
+++ tests/TestK2Preprocessor.java
@@ -0,0 +1,26 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Jonathan Zwiebel
+ * @version 1/25/16
+ * Experimental JUnit test
+ */
+public class TestK2Preprocessor {
+    @Test
+    public void testPass() {
+        Assert.assertTrue(true);
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testRead() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void thirdTest() {
+        Assert.assertTrue(false);
+    }
+
+}
