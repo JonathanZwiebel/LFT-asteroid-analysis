b7a1619505e7c9d35d73265c06c43128b4e36b4f
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2016-03-02 19:20:38 -0800

    Added reference frame parsing to LFMass

--- src/mains/LFBinREfMassFixedTime.java
+++ src/mains/LFBinREfMassFixedTime.java
@@ -1,5 +1,6 @@
 package mains;
 
+import filter.ReferenceFrameGenerationMethod;
 import locate.BinaryLocatorThresholdType;
 
 /**
@@ -18,11 +19,21 @@ public final class LFBinRefMassFixedTime {
         MEAN_SCALED_RANGE // specify scalar increment, low, and high count
     }
 
+    private enum ReferenceFrameMassType {
+        SINGLE, // specify type and arguments that go with it
+        GIVEN_RANGE, // specify increment, min, and max
+        MEAN_SHIFTED_RANGE, // specify increment, low, and high count
+        MEAN_SCALED_RANGE // specify scalar increment, low, and high count
+    }
+
     public static void run(String[] args) {
         assert args[0].equals("LF_MASS_FIXED_TIME");
 
         // TODO: Consider making this only accessible by method so every time it is checked, the number actually increments
         int current_arg = 1;
+
+        String filename = args[current_arg];
+        current_arg++;
 
         BinaryLocatorMassType binaryLocatorMassType = null;
         BinaryLocatorThresholdType binaryLocatorMassTypeSingleType = null; // this one is only filled in the SINGLE case
@@ -78,5 +89,67 @@ public final class LFBinRefMassFixedTime {
                 System.err.println("Illegal BinaryLocatorMassType: " + binaryLocatorMassTypeString);
                 System.exit(1);
         }
+
+        float similarity_threshold = Float.parseFloat(args[current_arg]);
+        current_arg++;
+
+        // TODO[Moderate]: refFrameMassType should be of a different enum then BinaryLocatorMassType
+        ReferenceFrameMassType refFrameMassType = null;
+        ReferenceFrameGenerationMethod refFrameMassTypeSingleType = null; // this one is only filled in the SINGLE case
+        float[] refFrameMassTypeArgs = null;
+        String refFrameMassTypeString = args[current_arg];
+        current_arg++;
+        switch(refFrameMassTypeString) {
+            case "SINGLE":
+                refFrameMassType = ReferenceFrameMassType.SINGLE;
+                String refFrameMassTypeSingleTypeString = args[current_arg];
+                current_arg++;
+                switch(refFrameMassTypeSingleTypeString) {
+                    case "BINARY_LOCATOR_ABSOLUTE":
+                        refFrameMassTypeSingleType = ReferenceFrameGenerationMethod.BINARY_LOCATOR_ABSOLUTE;
+                        refFrameMassTypeArgs = new float[]{Float.parseFloat(args[current_arg])};
+                        current_arg++;
+                        break;
+                    case "BINARY_LOCATOR_MEAN":
+                        refFrameMassTypeSingleType = ReferenceFrameGenerationMethod.BINARY_LOCATOR_MEAN;
+                        refFrameMassTypeArgs = new float[]{};
+                        break;
+                    case "BINARY_LOCATOR_MEAN_SHIFTED":
+                        refFrameMassTypeSingleType = ReferenceFrameGenerationMethod.BINARY_LOCATOR_MEAN_SHIFTED;
+                        refFrameMassTypeArgs = new float[]{Float.parseFloat(args[current_arg])};
+                        current_arg++;
+                        break;
+                    case "BINARY_LOCATOR_MEAN_SCALED":
+                        refFrameMassTypeSingleType = ReferenceFrameGenerationMethod.BINARY_LOCATOR_MEAN_SCALED;
+                        refFrameMassTypeArgs = new float[]{Float.parseFloat(args[current_arg])};
+                        current_arg++;
+                        break;
+                    default:
+                        System.err.println("Illegal binaryLocatorMassTypeSingleType: " + refFrameMassTypeSingleTypeString);
+                        System.exit(1);
+                }
+                break;
+            case "GIVEN_RANGE":
+                refFrameMassType = ReferenceFrameMassType.GIVEN_RANGE;
+                refFrameMassTypeArgs = new float[]{Float.parseFloat(args[current_arg]), Float.parseFloat(args[current_arg + 1]), Float.parseFloat(args[current_arg + 2])};
+                current_arg += 3;
+                break;
+            case "MEAN_SHIFTED_RANGE":
+                refFrameMassType = ReferenceFrameMassType.MEAN_SHIFTED_RANGE;
+                refFrameMassTypeArgs = new float[]{Float.parseFloat(args[current_arg]), Float.parseFloat(args[current_arg + 1]), Float.parseFloat(args[current_arg + 2])};
+                current_arg += 3;
+                break;
+            case "MEAN_SCALED_RANGE":
+                refFrameMassType = ReferenceFrameMassType.MEAN_SCALED_RANGE;
+                refFrameMassTypeArgs = new float[]{Float.parseFloat(args[current_arg]), Float.parseFloat(args[current_arg + 1]), Float.parseFloat(args[current_arg + 2])};
+                current_arg += 3;
+                break;
+            default:
+                System.err.println("Illegal RefFrameMassType: " + binaryLocatorMassTypeString);
+                System.exit(1);
+        }
+
+        int timestamp = Integer.parseInt(args[current_arg]);
+        current_arg++;
     }
 }
