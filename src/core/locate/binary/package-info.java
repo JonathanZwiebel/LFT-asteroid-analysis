8f5573c35228e80c4e11fb1fe91c1d505f079bca
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2016-07-12 11:35:52 -0700

    Added package-info file to core.locate.binary package

---
+++ src/core/locate/binary/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This core.locate.binary package contains classes for a concrete Locator and paired LocatorInstance class. The
+ * locating method used here is based on threshold filtering into binary images and adjacency grouping to determine
+ * BrightBody boundaries.
+ *
+ * @author Jonathan Zwiebel
+ * @version 11 July 2016
+ */
+package core.locate.binary;
