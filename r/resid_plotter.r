a2234ef471bb2c0470287e66f54d3aa9600ea2a6
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2016-07-21 16:41:43 -0700

    Replaced resid_analysis.r with resid_plotter.r

--- r/resid_plotter.r
+++ r/resid_plotter.r
@@ -1,29 +1,20 @@
-# Computes the outliers in a time series of an asteroid's brightness and
-# prints them. Outliers must have a Bonferroni value of 10.0 or lower.
+# Generates a plot of a the residual of a linear model of brightness from a time, flux that uses a 2nd
+# degree polynomial. Allows for restriction of data to account for jumps in flux values. 
 
 args = commandArgs(trailingOnly=TRUE)
 
 file_in = args[1]
-first_index = args[2]
-last_index = args[3]
+first_index = strtoi(args[2])
+last_index = strtoi(args[3])
 
 data = read.csv(file_in, header=FALSE)
 time = data$V1[first_index:last_index]
 flux = data$V2[first_index:last_index]
-
 time_squared = time * time
 
 model = lm(flux ~ time + time_squared)
 model_predictions = predict(model)
-
 resid = flux - model_predictions
-SSE = sum(resid ^ 2)
-
-writeLines("##########\nMODEL\n##########")
-summary(model)
-
-writeLines("##########\nERROR\n##########")
-paste("Sum squared error: ", SSE)
 
 # Plot
 X11()
